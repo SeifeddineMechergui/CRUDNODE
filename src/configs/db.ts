7d305bae91f8d69adac626b806c2531ac83fb34c
SeifeddineMechergui <saifmechergui2222@gmail.com>
2023-11-22 16:55:18 +0100

    fix bug

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const mongoURI = "mongodb+srv://seifeddine:B85mToSBEVYdGF6k@cluster0.o4udqpk.mongodb.net/shoping-service";
+    const mongoURI = "link mataa mongodb mtaek ";
     await mongoose.connect(mongoURI);
 
     console.log('MongoDB connected');
